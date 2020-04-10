944f897e574a4b9adb9c0bf0424b5884158b919a
Akhil Ravindran <imakhilravindran@gmail.com>
2020-04-10 23:13:34 +0530

    change sound in sound cloud player

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,21 @@ $('.gallery .owl-carousel').owlCarousel({
     },
     navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>']
 })
+
+// const client_id = 'OdgVKzzPYXN6cljKY5A6iOga2Cb0OUX';
+
+// const sound = new SoundCloudAudio(client_id);
+
+// sound.resolve('https://soundcloud.com/djangodjango/first-light', function(
+//   track
+// ) {
+//   // do smth with track object
+//   // e.g. display data in a view etc.
+//   console.log(track);
+ 
+//   // once track is loaded it can be played
+//   sound.play();
+ 
+//   // stop playing track and keep silence
+//   sound.pause();
+// });
